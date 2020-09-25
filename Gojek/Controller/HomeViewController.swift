8cc19872f2f7292f5b9d6b1a59487d0d8ef47053
Neeraj Solanki <neeraj.solanki@jeevansathi.com>
2020-09-25 23:37:17 +0530

    Repo Cell & Assets Added

--- Gojek/Controller/HomeViewController.swift
+++ Gojek/Controller/HomeViewController.swift
@@ -11,14 +11,32 @@ import UIKit
 class HomeViewController: UIViewController {
 
     // MARK: - Outlets
+    @IBOutlet weak var repoTableView: UITableView!
     
     // MARK: - Objetcs
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializeVariables()
     }
     
     // MARK: - Custom Methods
+    func initializeVariables() {
+        let nib = UINib.init(nibName: "RepoTableCell", bundle: nil)
+        repoTableView.register(nib, forCellReuseIdentifier: "RepoTableCell")
+    }
     
+}
+
+extension HomeViewController : UITableViewDelegate,UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let repoCell = tableView.dequeueReusableCell(withIdentifier: "RepoTableCell", for: indexPath) as! RepoTableCell
+        return repoCell
+    }
 }
---
+++ Gojek/Table Cell/RepoTableCell.swift
@@ -0,0 +1,27 @@
+//
+//  RepoTableCell.swift
+//  Gojek
+//
+//  Created by Neeraj Solanki on 25/09/20.
+//  Copyright © 2020 Mr.Solanki. All rights reserved.
+//
+
+import UIKit
+
+class RepoTableCell: UITableViewCell {
+
+    //MARK:- Outlets
+    
+    //MARK:- Life Cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    //MARK:- Custom Methods
+    
+}
